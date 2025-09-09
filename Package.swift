365a214dd285f8d38dc07988c870fff2b3e7fa8c
chenmo <chenmo@qq.copm>
2025-09-09 17:35:58 +0800

    11

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,7 @@ let package = Package(
     name: "Package_net",
     
     platforms: [
-        .iOS(.v15),
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
